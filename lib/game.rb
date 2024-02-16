6e1b41dee8a247895d43b3e7b68a26ea4f6b7905
Jared Hobson <jaredmhobson@gmail.com>
2024-02-16 15:27:28 -0800

    Update players attribute in game

--- lib/game.rb
+++ lib/game.rb
@@ -1,8 +1,19 @@
 class Game
-  attr_reader :current_player
+  attr_reader :players
 
   def initialize(board)
     @board = board
-    @current_player = nil
+    @players = []
+  end
+
+  def take_turn
+    # says whose turn it is
+    # prints current state of board
+    # requests a column
+    # checks to see if the user input is valid
+    # if valid, applies input to board
+    # once input is placed, checks for win conditions
+    # changes current_player
+    # loop
   end
 end
