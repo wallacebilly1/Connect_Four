6d6a6b66c784941a2a10eefbe3689c2c7d0be6c7
Billy Wallace <wallace.billy1@gmail.com>
2024-02-16 15:47:09 -0700

    Add #choose_column method to message class

--- lib/message.rb
+++ lib/message.rb
@@ -1,7 +1,11 @@
 class Message
 
-    def welcome_message
+    def welcome
         "Welcome to CONNECT FOUR\nEnter p to play, enter q to quit"
+    end
+
+    def choose_column
+        "Please choose a column (A-G) to place your piece."
     end
 
 end
