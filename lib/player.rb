6f19922383b870d28353b99973abf1ab6af4b7e8
Billy Wallace <wallace.billy1@gmail.com>
2024-02-16 14:59:01 -0700

    Add #choose_piece method and piece instance variable

--- lib/player.rb
+++ lib/player.rb
@@ -1,8 +1,17 @@
 class Player
-	attr_reader :name
+	attr_reader :name, :piece
 
 	def initialize(name = "Computer")
 		@name = name
+		@piece = choose_piece
+	end
+
+	def choose_piece
+		if name == "Computer"
+			@piece = "O"
+		else 
+			@piece = "X"
+		end
 	end
 
 end
