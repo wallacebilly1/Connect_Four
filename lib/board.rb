cd3eb032c181adfbc92c9517de8b2a28febab78d
Jared Hobson <jaredmhobson@gmail.com>
2024-02-15 17:26:35 -0800

    Add board attr_reader and instance variable in board

--- lib/board.rb
+++ lib/board.rb
@@ -1,2 +1,15 @@
 class Board
+  attr_reader :board
+
+  def initialize
+    @board = {
+      A: %w[. . . . . .],
+      B: %w[. . . . . .],
+      C: %w[. . . . . .],
+      D: %w[. . . . . .],
+      E: %w[. . . . . .],
+      F: %w[. . . . . .],
+      G: %w[. . . . . .]
+    }
+  end
 end
