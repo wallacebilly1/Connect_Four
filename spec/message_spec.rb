768ebc6e57f6b073c22c2c6482f78f6975152c7d
Billy Wallace <wallace.billy1@gmail.com>
2024-02-16 15:46:15 -0700

    Add test for #choose_column method in message_spec

--- spec/message_spec.rb
+++ spec/message_spec.rb
@@ -5,15 +5,19 @@ RSpec.describe Message do
     let (:message) { Message.new }
 
     describe '#initialize' do
-
         it 'exists' do
             expect(message).to be_an_instance_of Message
         end
-
+    end
+    describe '#welcome' do
         it 'properly displays welcome message' do
-            expect(message.welcome_message).to be_an_instance_of String
+            expect(message.welcome).to be_an_instance_of String
         end
-        
     end
 
+    describe '#choose_column' do
+        it 'properly displays choose_column message' do
+            expect(message.choose_column).to be_an_instance_of String
+        end
+    end
 end
