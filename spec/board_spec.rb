32c03d12319563ab0e7eb3087fc6909b5dc2c6f9
Jared Hobson <jaredmhobson@gmail.com>
2024-02-15 17:12:54 -0800

    Add instantiations and exists test in board_spec

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,1 +1,10 @@
 require './spec_helper.rb'
+
+RSpec.describe Board do
+  let (:board) { Board.new }
+  describe '#initialize' do
+    it 'exists' do
+      expect(board).to be_instance_of(Board)
+    end
+  end
+end
