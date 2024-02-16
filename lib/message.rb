41c025d49084198576ba620004c1283079d57356
Grant Davis <grantdavis303@gmail.com>
2024-02-16 13:30:11 -0700

    Add Message Class + RSpec Initialize Test

---
+++ lib/message.rb
@@ -0,0 +1,8 @@
+class Message
+
+    def welcome_message
+        puts "Welcome to CONNECT FOUR"
+        puts "Enter p to play, enter q to quit"
+    end
+
+end
--- spec/message_spec.rb
+++ spec/message_spec.rb
@@ -2,6 +2,17 @@ require 'spec_helper.rb'
 
 RSpec.describe Message do
 
+    let (:message) { Message.new }
 
+    describe '#initialize' do
+
+        it 'exists' do
+            expect(message).to be_an_instance_of Message
+        end
+
+        it 'properly displays welcome message' do
+        end
+
+    end
 
 end
