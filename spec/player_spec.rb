f0aee8593e2ebe8efe08e99979ad201a603446e4
Billy Wallace <wallace.billy1@gmail.com>
2024-02-16 14:26:02 -0700

    Add test for initialize

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -1,7 +1,13 @@
 require 'spec_helper.rb'
 
 RSpec.describe Player do
+	
+	let (:player) { Player.new }
 
-
+	describe '#initialize' do
+    it 'exists' do
+      expect(player).to be_instance_of(Player)
+    end
+  end
 
 end
