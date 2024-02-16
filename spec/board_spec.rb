19762a3bcdcf3e58432ebb1c680f1537c82c82a5
Jared Hobson <jaredmhobson@gmail.com>
2024-02-15 17:24:41 -0800

    Add test for board attr_reader in board_spec

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,10 +1,14 @@
-require './spec_helper.rb'
+require 'spec_helper.rb'
 
 RSpec.describe Board do
   let (:board) { Board.new }
   describe '#initialize' do
     it 'exists' do
       expect(board).to be_instance_of(Board)
+    end
+
+    it 'creates a board when initialized' do
+      expect(board.board).to be_instance_of(Hash)
     end
   end
 end
