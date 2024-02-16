bf08c99cdab007a58681c89171979bb83d20eb69
Jared Hobson <jaredmhobson@gmail.com>
2024-02-16 15:09:59 -0800

    Add instantiation and test for intialize method in game_spec

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,6 +2,11 @@ require 'spec_helper.rb'
 
 RSpec.describe Game do
 
+  let (:game) {Game.new(Board.new)}
 
-
+  describe '#initialize' do
+    it 'exists' do
+      expect(game).to be_instance_of(Game)
+    end
+  end
 end
