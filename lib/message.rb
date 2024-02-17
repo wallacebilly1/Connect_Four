9a9aa76fb86bb02fde9898e56207d5fde5da7bb7
Grant Davis <grantdavis303@gmail.com>
2024-02-17 11:30:06 -0700

    Create message for invalid input after game start

--- lib/message.rb
+++ lib/message.rb
@@ -3,6 +3,10 @@ class Message
     "Welcome to CONNECT FOUR\nEnter p to play, enter q to quit."
   end
 
+  def invalid_command
+    "Invalid command. Please enter either 'p' or 'q'"
+  end
+
   def choose_column
     "Please choose a column (A-G) to place your piece."
   end
