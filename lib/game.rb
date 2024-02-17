a6dff9b4dac578a2161e30de283a8903e7fa6f4a
Grant Davis <grantdavis303@gmail.com>
2024-02-17 13:43:36 -0700

    Update take turn method

--- lib/game.rb
+++ lib/game.rb
@@ -5,53 +5,54 @@ class Game
     @board = Board.new
     @players = create_players
     @message_bot = Message.new
+    @turns_taken = 0
   end
 
   def start
     puts @message_bot.welcome
+    play = true   
     answer = gets.chomp.downcase
-    condition = false
-    until condition == true do
-      if answer == "p"
-        condition = true
+        if answer == 'q'
+            play = false
+        elsif answer == 'p'
+            play
+        else
+          puts @message_bot.invalid_command
+          answer = gets.chomp.downcase
+        end    
+    while play
+        puts @board.print_visual
         take_turn
-      elsif answer == "q"
-        condition = true
-        exit
-      else
-        puts @message_bot.invalid_command
-        answer = gets.chomp.downcase
-      end
+        change_current_player
+        # Move later bros
+        puts @message_bot.last_piece_played(@board.last_piece_played)
     end
   end
 
   def take_turn
-    # says whose turn it is
+    puts "-----------------------------------------"
     puts @message_bot.current_player_turn(current_player.name)
-    # prints current state of board
-    puts @board.print_visual
-    # requests a column
-    answer = false
-    until answer == true do
-      puts @message_bot.choose_column        
-      column_choice = current_player.choose_column.upcase
-      # checks to see if the user input is valid
-      answer = is_valid?(column_choice)        
+    column_choice =
+    if current_player.name == "Player"
+        player_take_turn
+    else
+        computer_take_turn
     end
-
-    # if valid, applies input to board
     @board.update_board(column_choice, current_player)
-    @board.print_visual
+  end
 
-    # once input is placed, checks for win conditions
-
-    # changes current_player
-    change_current_player
-    # loop    
-    take_turn
+  def player_take_turn
+    answer = false
+    while answer == false do
+      puts @message_bot.choose_column
+      column_choice = current_player.choose_column.upcase
+      answer = is_valid?(column_choice)  
+    end
+    column_choice     
   end
 
   def computer_take_turn
+    sleep(2)
     %w[A B C D E F G].select do |column|
       is_valid?(column)
     end.sample
