ec0666ff53ff26d05a24836bb2e4d9c1c61c1110
Grant Davis <grantdavis303@gmail.com>
2024-02-16 17:51:05 -0700

    Add a message for current player turn and RSpec

--- lib/message.rb
+++ lib/message.rb
@@ -1,11 +1,15 @@
 class Message
 
     def welcome
-        "Welcome to CONNECT FOUR\nEnter p to play, enter q to quit"
+        "Welcome to CONNECT FOUR\nEnter p to play, enter q to quit."
     end
 
     def choose_column
         "Please choose a column (A-G) to place your piece."
+    end
+
+    def current_player_turn(player)
+        "It is currently #{player}'s turn."
     end
 
 end
--- spec/message_spec.rb
+++ spec/message_spec.rb
@@ -9,15 +9,17 @@ RSpec.describe Message do
             expect(message).to be_an_instance_of Message
         end
     end
-    describe '#welcome' do
+    describe '#message methods' do
         it 'properly displays welcome message' do
             expect(message.welcome).to be_an_instance_of String
         end
-    end
 
-    describe '#choose_column' do
         it 'properly displays choose_column message' do
             expect(message.choose_column).to be_an_instance_of String
         end
+
+        it 'properly displays the current_player' do
+            expect(message.current_player_turn("Player")).to be_an_instance_of String
+        end 
     end
 end
